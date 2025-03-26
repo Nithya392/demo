321557fc6a8bf764012b387727b61f35697a8632
Nithya392 <nithyashree.b@s.amity.edu>
2025-03-26 20:26:54 +0530

    started while loop

--- while.c
+++ while.c
@@ -6,4 +6,20 @@ int main() {
     printf("Welcome to the Vending Machine!\n");
     printf("Item Price: %d cents\n", price);
 
-    
+    while (amount < price) {
+        printf("Insert coin (5, 10, 25, 50, 100 cents, 0 to cancel): ");
+        scanf("%d", &coin);
+
+     
+        if (coin == 0) {
+            printf("Transaction canceled. Returning %d cents.\n", amount);
+            return 0;
+        }
+
+        
+        if (coin != 5 && coin != 10 && coin != 25 && coin != 50 && coin != 100) {
+            printf("Invalid coin. Try again.\n");
+            continue;
+        }
+
+       
